9838cf542edd6fe99c7179f3992ed7e11a41fff9
NiCo7889 <113980882+NiCo7889@users.noreply.github.com>
2022-12-11 18:06:25 +0000

    cambio

--- EJERCICIO1.py
+++ EJERCICIO1.py
@@ -7,15 +7,15 @@ def main():
 
     # Cargar como dataframe de pandas el csv imdb_elenco.csv y mostrar sus 5 primeros registros
     elenco = pd.read_csv('imdb_elenco.csv')
-    print(elenco.head(5))
+    print(elenco.head())
 
     # Mostrar el número de registros del dataframe de titulos
+    print("Mostrar el número de registros del dataframe de titulos{} ".format(len(titulos_de_peliculas.shape)))
 
     # Mostrar el número de registros del dataframe de elenco
+    print("Mostrar el número de registros del dataframe de elenco{} ".format(len(elenco.shape)))
 
     # Mostrar las 5 peliculas más antiguas del listado de titulos
-
-
 
 
 
