cdb201a09766258407162577b79d53cadfbab281
NiCo7889 <113980882+NiCo7889@users.noreply.github.com>
2022-12-11 16:38:41 +0000

    cambio

--- EJERCICIO1.py
+++ EJERCICIO1.py
@@ -1,6 +1,11 @@
 import pandas as pd 
 
 def main():
+    titulos_de_peliculas = pd.read_csv('imdb_titulos.csv')
+    print(titulos_de_peliculas.head(5))
+
+    elenco = pd.read_csv('imdb_elenco.csv')
+    print(elenco.head(5))
 
 
 
