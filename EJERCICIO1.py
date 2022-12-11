ab281af577198af6819bf382e72e0352fac7e3cf
NiCo7889 <113980882+NiCo7889@users.noreply.github.com>
2022-12-11 17:07:27 +0000

    c

--- EJERCICIO1.py
+++ EJERCICIO1.py
@@ -1,14 +1,21 @@
-import pandas as pd 
+import pandas as pd # Importa pandas y usa un alias para poder utilizar sus recursos
 
 def main():
+    # Cargar como dataframe de pandas el csv imdb_titulos.csv y mostrar sus 5 primeros registros
     titulos_de_peliculas = pd.read_csv('imdb_titulos.csv')
     print(titulos_de_peliculas.head(5))
 
+    # Cargar como dataframe de pandas el csv imdb_elenco.csv y mostrar sus 5 primeros registros
     elenco = pd.read_csv('imdb_elenco.csv')
     print(elenco.head(5))
 
+    # Mostrar el número de registros del dataframe de titulos
+
+    # Mostrar el número de registros del dataframe de elenco
+
+    # Mostrar las 5 peliculas más antiguas del listado de titulos
 
 
 
-if __name__ == '__main__':
-    main()
+
+
