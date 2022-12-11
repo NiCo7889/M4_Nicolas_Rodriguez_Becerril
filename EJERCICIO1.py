8aa80fc21e1a178f7771afe391fd732387cb886a
NiCo7889 <113980882+NiCo7889@users.noreply.github.com>
2022-12-11 18:16:15 +0000

    cc

--- EJERCICIO1.py
+++ EJERCICIO1.py
@@ -16,6 +16,13 @@ def main():
     print("Mostrar el número de registros del dataframe de elenco{} ".format(len(elenco.shape)))
 
     # Mostrar las 5 peliculas más antiguas del listado de titulos
+    print(titulos_de_peliculas.sort_values(by='año').head(5))
+
+    # Mostrar las peliculas que en el titulo tienen la palabra "Dracula". También mostrar el número total de peliculas que coincidan con este requisito
+    print(titulos_de_peliculas[titulos_de_peliculas['titulo'].str.contains('Dracula')])
+    print("Mostrar las peliculas que en el titulo tienen la palabra Dracula. También mostrar el número total de peliculas que coincidan con este requisito{} ".format(len(titulos_de_peliculas[titulos_de_peliculas['titulo'].str.contains('Dracula')])))
+    print(titulos_peliculas[titulos_peliculas['title'].str.contains('Dracula')])  #str.contains() devuelve la subcadena indicada en el parámetro
+    print("Número de peliculas que coincidan con este requisito: {} coincidencias".format(len(titulos_peliculas[titulos_peliculas['title'].str.contains('Dracula')])), "\n")
 
 
 
